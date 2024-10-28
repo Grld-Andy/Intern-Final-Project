bb02d13865ebd8a1eb249257e0482b235f33da4e
Andy-Ansong <andy.ansong@amalitechtraining.org>
2024-10-28 14:54:51 +0000

    styles project preview page

--- frontend/src/pages/ProjectPreview/ProjectPreview.tsx
+++ frontend/src/pages/ProjectPreview/ProjectPreview.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import "./styles.css"
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined"
 import CheckCircleIcon from "@mui/icons-material/CheckCircle"
-import PlayCircleFilledOutlinedIcon from "@mui/icons-material/PlayCircleFilledOutlined"
 import LinkOutlinedIcon from "@mui/icons-material/LinkOutlined"
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline"
 import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined"
@@ -10,27 +9,28 @@ import ArrowOutwardOutlinedIcon from "@mui/icons-material/ArrowOutwardOutlined"
 
 const ProjectPreview: React.FC = () => {
   return (
-    <div className="text-[#344054] bg-[#F9FAFB]">
+    <div className="project-preview bg-[#F9FAFB]">
       <div className="hero-image"></div>
       <div className="page-container">
         <div className="header">
-          <h1>Event planner</h1>
-          <div>
+          <h1>Event seating planner</h1>
+          <div className="calendar">
             <div className="time-list">
-              <div className="time">
-                <p><CalendarTodayOutlinedIcon/>Created 17 Sep, 2024</p>
-                <p>Last modified 17 hours ago</p>
+              <div>
+                <CalendarTodayOutlinedIcon/>
+                <span>Created 17 Sep, 2024</span>
               </div>
-              <button>Edit project</button>
+              <p>Last modified 17 hours ago</p>
             </div>
+            <button>Edit project</button>
           </div>
         </div>
 
-        <div>
           <div className="bottom">
             <div className="left">
               <div className="left-top">
                 <h3>Development Stack</h3>
+                <div className="stacks">
                 {
                   Array.from({length: 4}).map((_, index) => (
                     <div className="stack" key={index}>
@@ -38,11 +38,14 @@ const ProjectPreview: React.FC = () => {
                     </div>
                   ))
                 }
+                </div>
               </div>
               <div className="project-description">
                 <h1>Project description</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet laborum nam cum, blanditiis quas sed, aspernatur eligendi harum inventore labore dolorum, possimus nulla esse sit repellat omnis dolorem dignissimos libero atque iste autem repellendus? Modi iusto at placeat unde est corrupti, quod quia perferendis ab obcaecati earum, voluptas temporibus tenetur? Tempora culpa neque repellat placeat unde, molestiae possimus consectetur magni?</p>
-                <span>See more...</span>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet laborum nam cum, blanditiis quas sed, aspernatur eligendi harum inventore labore dolorum, possimus nulla esse sit repellat omnis dolorem dignissimos libero atque iste autem repellendus? Modi iusto at placeat unde est corrupti, quod quia perferendis ab obcaecati earum, voluptas temporibus tenetur? Tempora culpa neque repellat placeat unde, molestiae possimus consectetur magni?
+                  <span>See more...</span>
+                </p>
               </div>
               <div className="project-features">
                 <h1>Project features</h1>
@@ -74,14 +77,18 @@ const ProjectPreview: React.FC = () => {
                 <h1>Technical details and Decisions</h1>
                 <div className="video">
                   <video src="" controls></video>
-                  <PlayCircleFilledOutlinedIcon/>
                 </div>
               </div>
             </div>
 
+            <div className="middle"></div>
+
             <div className="right">
               <div className="right-top">
-                <h1><LinkOutlinedIcon/>Linked Docs</h1>
+                <div className="right-title">
+                  <LinkOutlinedIcon/>
+                  <h1>Linked Docs</h1>
+                </div>
                 <div className="input-div">
                   <input type="text" placeholder="Eventplanner_documentation_here.pdf"/>
                   <ArrowOutwardOutlinedIcon/>
@@ -92,13 +99,16 @@ const ProjectPreview: React.FC = () => {
                 </div>
               </div>
               <div className="right-bottom">
-                <h1><GroupOutlinedIcon/>Development team/Contributors</h1>
+                <div className="right-title">
+                  <GroupOutlinedIcon/>
+                  <h1>Development team/Contributors</h1>
+                </div>
                 <div className="team-list">
                   {
                     Array.from({length: 4}).map((_, index) => (
                       <div className="member" key={index}>
                         <div className="image-container">
-                          <img src=""/>
+                          <img src="/Projects_preview_page/frontend.jpg"/>
                         </div>
                         <div className="member-details">
                           <h1>Member Name</h1>
@@ -112,7 +122,6 @@ const ProjectPreview: React.FC = () => {
               </div>
             </div>
           </div>
-        </div>
       </div>
     </div>
   )
